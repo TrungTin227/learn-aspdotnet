e0a8ef1ac1e14260208d43738d84b3c10358236d
VuTrungTin <tinvtse161572@fpt.edu.vn>
2024-12-25 21:17:14 +0700

    Learn ASP.NET Controller and Action Method

--- WebApplication1/WebApplication1/Controllers/Math.cs
+++ WebApplication1/WebApplication1/Controllers/Math.cs
@@ -4,9 +4,12 @@ namespace WebApplication1.Controllers
 {
     public class Math : Controller
     {
-        public IActionResult Sum(int x, int y)
+        public IActionResult Sum(int x, int y)// public string Sum(int x, int y)
         {
             return Content((x + y).ToString());
-        }
+        } // return (x + y).ToString();
+        //nếu hàm action method trả về kiểu IActionResult thì nó sẽ trả về 1 view(Kiểu dữ liệu sẽ tùy chỉnh vào dữ liệu như var)
+        //nếu kiểu trả về k phải kiểu IActionResult thì nó sẽ trả về kiểu mà chúng ta đã khai báo + ToString() nó sẽ gửi nguyên mẫu của kiểu dữ liệu đó về cho client
+        //Trong thực tế ta sẽ không thường trực tiếp trả về 1 kiểu như int, string mà sẽ trả về  kiểu IActionResult
     }
 }
