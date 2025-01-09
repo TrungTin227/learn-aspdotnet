6b4dca801e1aaa66f45b8dd35550ec1435dec4a8
VuTrungTin <tinvtse161572@fpt.edu.vn>
2025-01-09 16:28:40 +0700

    Learn HttpContext

---
+++ HttpContextDemo/HttpContextDemo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using HttpContextDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using HttpContextDemo.Helpers;
+
+namespace HttpContextDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //HttpContext.Request được implement trong Controller base class vì vậy không cần thêm bất kỳ thư viện nào vd: HttpContext.Request hoặc viết ngắn gọn là Request
+            //_logger.LogInformation("ID {id} from {sch}://{host}", HttpContext.TraceIdentifier, Request.Scheme, Request.Host);
+            //Dài dòng nên khai báo riêng 1 hàm ở dưới
+            _logger.LogInformation("ID {id} from {host}", HttpContext.TraceIdentifier, Request.GetDebugInfo());
+            return View();
+        }
+
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HttpContextDemo/HttpContextDemo/Helpers/RequestExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace HttpContextDemo.Helpers
+{
+    //Viết 1 hàm chuyên để viết các Extension method thì phải dùng static class
+    public static class RequestExtensions
+    {
+        public static string GetDebugInfo(this HttpRequest request)
+        {
+            return $"{request.Scheme}://{request.Host}";
+        }
+    }
+}
