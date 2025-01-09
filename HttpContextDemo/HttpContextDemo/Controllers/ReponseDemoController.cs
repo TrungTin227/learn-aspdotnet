60d59ffeb4e83b89ba0d077fe904440632f85087
VuTrungTin <tinvtse161572@fpt.edu.vn>
2025-01-09 22:43:32 +0700

    Learn HttpContext, Request, Response

---
+++ HttpContextDemo/HttpContextDemo/Controllers/ReponseDemoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpContextDemo.Controllers
+{
+    public class ReponseDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            Response.Headers.Append("X-Test-Header", "TestHeaderValue");
+            return Ok("Test");
+        }
+    }
+}
