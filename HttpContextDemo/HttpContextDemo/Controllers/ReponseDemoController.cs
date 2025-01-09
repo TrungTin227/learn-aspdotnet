a31ed4325f91b8d0e26a92eeb1413a41a76a97f4
VuTrungTin <tinvtse161572@fpt.edu.vn>
2025-01-09 22:44:05 +0700

    Learn HttpContext, Request, Response

--- HttpContextDemo/HttpContextDemo/Controllers/ReponseDemoController.cs
+++ HttpContextDemo/HttpContextDemo/Controllers/ReponseDemoController.cs
@@ -13,6 +13,27 @@ namespace HttpContextDemo.Controllers
         public IActionResult Test()
         {
             Response.Headers.Append("X-Test-Header", "TestHeaderValue");
+
+            FileInfo file = new FileInfo("wwwroot/images/1.jpg");
+            Response.ContentType = "text/html";
+            Response.ContentLength = file.Length;
+
+            using (var stream = file.OpenRead())
+            {
+                //var buffer = new byte[4096];
+                //int bytesRead;
+                //while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                //{
+                //    Response.Body.Write(buffer, 0, bytesRead);
+                //}
+                //Dùng cách trên làm tốn bộ nhớ hơn khi đọc file lớn
+
+                //Cách viết ngắn gọn hơn khi sử dụng stream và dùng CopyTo để copy từ stream này sang stream khác thông qua Response.Body
+
+                stream.CopyTo(Response.Body);
+            }
+
+
             return Ok("Test");
         }
     }
